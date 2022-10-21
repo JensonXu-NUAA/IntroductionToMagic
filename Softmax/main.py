e7fd39e207c26066b80369fd2ccafc1b623da489
魔法少女☆TeRiRi <55434673+Jenson2525@users.noreply.github.com>
2022-10-21 23:49:54 +0800

    Update main.py

--- Softmax/main.py
+++ Softmax/main.py
@@ -1,7 +1,8 @@
-from matplotlib.pyplot import axis
 import torch
-from IPython import display
 from d2l import torch as d2l
+import matplotlib.pyplot as plt
+from accumulator import Accumulator
+from animator import Animator
 
 '''
 softmax回归，实现对Fashion-MNIST数据集的分类
@@ -9,62 +10,6 @@ softmax回归，实现对Fashion-MNIST数据集的分类
 
 batch_size = 256  # 设置数据迭代器的批量⼤⼩为256
 train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)  # 读取Fashion-MNIST数据集
-
-# 定义Accumulator类，用于对多个变量进行累加
-class Accumulator:
-    def __init__(self, n):
-        self.data = [0.0] * n
-    
-    def add(self, *args):
-        self.data = [a + float(b) for a, b in zip(self.data, args)]
-
-    def reset(self):
-        self.data = [0, 0] * len(self.data)
-
-    def __getitem__(self, index):
-        return self.data[index]
-
-# 定义Animator类，在动画中绘制数据
-class Animator:
-    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
-                 ylim=None, xscale='linear', yscale='linear',
-                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1, figsize=(3.5, 2.5)):
-        # 增量地绘制多条线
-        if legend is None:
-            legend = []
-        d2l.use_svg_display()
-        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
-        if nrows * ncols == 1:
-            self.axes = [self.axes, ]
-
-        # 使用lambda函数捕获参数
-        self.config_axes = lambda: d2l.set_axes(self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
-        self.X, self.Y, self.fmts = None, None, fmts
-
-    # 向图表中添加多个数据点
-    def add(self, x, y):
-        if not hasattr(y, "__len__"):
-            y = [y]
-        n = len(y)
-        if not hasattr(x, "__len__"):
-            x = [x] * n
-        if not self.X:
-            self.X = [[] for _ in range(n)]
-        if not self.Y:
-            self.Y = [[] for _ in range(n)]
-        for i, (a, b) in enumerate(zip(x, y)):
-            if a is not None and b is not None:
-                self.X[i].append(a)
-                self.Y[i].append(b)
-        self.axes[0].cla()
-        for x, y, fmt in zip(self.X, self.Y, self.fmts):
-            self.axes[0].plot(x, y, fmt)
-        self.config_axes()
-        display.display(self.fig)
-        display.clear_output(wait=True)
-    
-    def show_figure(self):
-        d2l.plt.show()
 
 # 定义softmax操作
 def softmax(X):
@@ -85,23 +30,6 @@ def cross_entropy(y_hat, y):
 lr = 0.1
 def updater(batch_size):
     return d2l.sgd([W, b], lr, batch_size)
-
-# 计算分类精度
-def accuracy(y_hat, y):
-    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
-        y_hat = y_hat.argmax(axis=1)
-    cmp = y_hat.type(y.dtype) == y  # 强制类型转换
-    return float(cmp.type(y.dtype).sum())
-
-# 计算在指定数据集上模型的精度
-def evaluate_accuracy(model, data_iter):
-    if isinstance(model, torch.nn.Module):
-        model.eval() # 将模型设置为评估模式
-    metric = Accumulator(2) # 正确预测数、预测总数
-    with torch.no_grad():
-        for X, y in data_iter:
-            metric.add(accuracy(model(X), y), y.numel())
-    return metric[0] / metric[1]
 
 # 定义训练
 def train(model, train_iter, loss, updater):
@@ -124,7 +52,7 @@ def train(model, train_iter, loss, updater):
         else:
             l.sum().backward()
             updater(X.shape[0])
-        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+        metric.add(float(l.sum()), d2l.accuracy(y_hat, y), y.numel())
     # 返回训练损失和训练精度
     return metric[0] / metric[2], metric[1] / metric[2]
 
@@ -136,7 +64,7 @@ def train_softmax(model, train_iter, test_iter, loss, num_epochs, updater):
     
     for epoch in range(num_epochs):
         train_metrics = train(model, train_iter, loss, updater)
-        test_acc = evaluate_accuracy(model, test_iter)
+        test_acc = d2l.evaluate_accuracy(model, test_iter)
         animator.add(epoch + 1, train_metrics + (test_acc, ))
     train_loss, train_acc = train_metrics
     assert train_loss < 0.5, train_loss
@@ -151,7 +79,7 @@ def predict(model, test_iter, n=5):
     preds = d2l.get_fashion_mnist_labels(model(X).argmax(axis=1))
     titles = [true + '\n' + pred for true, pred in zip(trues, preds)]
     d2l.show_images(X[0:n].reshape((n, 28, 28)), 1, n, titles=titles[0:n])
-    d2l.plt.show()
+    plt.show()
 
 '''
 X = torch.normal(0, 1, (2, 5))
