6c2446512fc76365da746de4e288795eef480ced
魔法少女☆TeRiRi <55434673+Jenson2525@users.noreply.github.com>
2022-11-14 00:24:26 +0800

    Create main.py

---
+++ 卷积神经网络(CNN)/NiN/main.py
@@ -0,0 +1,47 @@
+import torch
+import matplotlib.pyplot as plt
+from torch import nn 
+from d2l import torch as d2l
+
+'''
+实现一个NiN网络
+'''
+
+# 构造一个NiN块
+def nin_block(in_channels, out_channels, kernel_size, strides, padding):
+    # 一个NiN块由3个卷积层组成
+    block = nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size, strides, padding), nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1), nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1), nn.ReLU()
+    )
+    return block
+
+# 设计网络，添加了4个NiN块
+net = nn.Sequential(
+    nin_block(1, 96, kernel_size=11, strides=4, padding=0),
+    nn.MaxPool2d(3, stride=2),
+    nin_block(96, 256, kernel_size=5, strides=1, padding=2),
+    nn.MaxPool2d(3, stride=2),
+    nin_block(256, 384, kernel_size=3, strides=1, padding=1),
+    nn.MaxPool2d(3, stride=2),
+    nn.Dropout(0.5),
+
+    # 输出层
+    nin_block(384, 10, kernel_size=3, strides=1, padding=1),
+    nn.AdaptiveAvgPool2d((1, 1)),
+    nn.Flatten()  # 将四维的输出转成⼆维的输出，其形状为(批量⼤⼩,10)
+)
+
+'''
+# 测试
+X = torch.rand(size=(1, 1, 224, 224))
+for layer in net:
+    X = layer(X)
+    print(layer.__class__.__name__,'output shape:\t', X.shape)
+'''
+
+lr, num_epochs, batch_size = 0.1, 10, 128
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=224)
+d2l.train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
+plt.show()
